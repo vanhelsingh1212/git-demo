f8e72ddf2a68faaa6c1f8c11a63291e33493fbb7
venkatesh <yarlagaddavenkatesh1994@gmail.com>
2024-11-09 23:05:19 +0530

    new push

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
      }
 
         System.out.println("venkatesh this is demo one please watch carefully new one tryied");
-        System.out.println("new pull data");
+        System.out.println("new push data one handle with careg");
 
     }
 }
